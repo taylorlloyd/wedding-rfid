796adac987014aba41a05e6eab8c28fdc0d384c6
Taylor Lloyd <taylor@taylorlloyd.ca>
2018-01-24 16:46:23 -0700

    fix invalid references to Activation

--- tableview.py
+++ tableview.py
@@ -19,16 +19,16 @@ class TableWindow(Window):
         self.launch(PasswordWindow())
 
     def setActive(self, surface, manager):
-        super(Activation, self).setActive(surface, manager)
+        super(TableWindow, self).setActive(surface, manager)
         time.sleep(3.5)
         self.finish()
 
     def __init__(self, name, table):
-        super(Activation, self).__init__(background_color=white)
+        super(TableWindow, self).__init__(background_color=white)
         self.name = TextView(name, font=name_font)
         self.table = TextView("Table", font=table_font)
         self.tnum = TextView(str(table), font=tnum_font)
-        self.addView(Rect(0,30,320,60), self.name)
+        self.addView(Rect(0,00,320,80), self.name)
         self.addView(Rect(0,160,320,60), self.table)
         self.addView(Rect(0,220,320,100), self.tnum)
 
